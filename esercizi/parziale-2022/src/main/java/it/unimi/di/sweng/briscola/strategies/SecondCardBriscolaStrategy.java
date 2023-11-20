2fa144a7070a74d53adebdc2e0e3fcb9ae1fa9f4
MartinWWDC <60852345+MartinWWDC@users.noreply.github.com>
2023-11-20 18:05:04 +0100

    ROSSO: secondCardBriscolaStrategyTest non funziona

---
+++ esercizi/parziale-2022/src/main/java/it/unimi/di/sweng/briscola/strategies/SecondCardBriscolaStrategy.java
@@ -0,0 +1,20 @@
+package it.unimi.di.sweng.briscola.strategies;
+
+import it.unimi.di.sweng.briscola.Card;
+import it.unimi.di.sweng.briscola.Player;
+import it.unimi.di.sweng.briscola.Strategy;
+import it.unimi.di.sweng.briscola.Suit;
+import org.jetbrains.annotations.NotNull;
+
+public class SecondCardBriscolaStrategy implements Strategy {
+    private Strategy next;
+
+    public SecondCardBriscolaStrategy(Strategy next) {
+        this.next = next;
+    }
+
+    @Override
+    public @NotNull Card chooseCard(@NotNull Player me, @NotNull Player other, @NotNull Suit briscola) {
+        return null;
+    }
+}
